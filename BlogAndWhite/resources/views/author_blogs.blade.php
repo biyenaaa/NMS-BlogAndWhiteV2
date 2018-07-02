557460c17fbc20c585530c941f9d93e2391e9ee0
chaserylle <oneandonlychasey@gmail.com>
2018-07-02 10:49:54 +0800

    #14 message for accounts with no blogs yet

--- BlogAndWhite/resources/views/author_blogs.blade.php
+++ BlogAndWhite/resources/views/author_blogs.blade.php
@@ -3,21 +3,25 @@
 @section('title', 'My Blogs')
 
 @section('content')
-
-	<div class="container-fluid">
-		<div class="accordion" id="accordionExample">
-			@foreach ($posts as $post)
-				<div class="container">
-					<h4>{{ $post->title }} </h4>
-					<p>{{ $post->date_published }} </p>
-					<button type="button" href="#" class="btn btn-info" data-toggle="collapse" data-target="#demo">View</button>
-					<div id="demo" class="collapse">
-						{{ $post->content }}
+	@if (count($posts)==0)
+		<div style="margin-top: 10%;" class="container"><center> You have no blogs yet </center></div>
+		<center><a class="nav-link" href="{!! url('/blog_form'); !!}"> Create My First Blog <span class="sr-only"></span></a></center>
+	@else
+		<div class="container-fluid">
+			<div class="accordion" id="accordionExample">
+				@foreach ($posts as $post)
+					<div class="container">
+						<h4>{{ $post->title }} </h4>
+						<p>{{ $post->date_published }} </p>
+						<button type="button" href="#" class="btn btn-info" data-toggle="collapse" data-target="#demo">View</button>
+						<div id="demo" class="collapse">
+							{{ $post->content }}
+						</div>
 					</div>
-				</div>
-			@endforeach
+				@endforeach
 
+			</div>
 		</div>
-	</div>
+	@endif
 	
 @stop
