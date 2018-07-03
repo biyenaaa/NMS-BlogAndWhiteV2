75b1f5908b7b3a86709b6763be335f49c15e16f3
Aika <aikaviend@gmail.com>
2018-07-03 09:09:11 +0800

    #9 specific error messages.

--- BlogAndWhite/app/Http/Controllers/RegistrationController.php
+++ BlogAndWhite/app/Http/Controllers/RegistrationController.php
@@ -15,53 +15,50 @@ class RegistrationController extends Controller {
 
 	public static function add_account(Request $request) {
 
-		$validator = Validator::make(Input::all(), [
+		$usernamevalidator = Validator::make(Input::all(), [
 			'username' => 'required',
 	 		'email' => 'required|email',
 	 		//'password' => 'required|min:8|confirmed'
 		]);
 
-		$validator1 = Validator::make(Input::all(), [ 
+		//dd($data['username']);
+
+		$pwlengthvalidator = Validator::make(Input::all(), [ 
 			'password' => 'required|min:8'
 		]);
 
-		$validator2 = Validator::make(Input::all(), [
+		$pwconfirmvalidator = Validator::make(Input::all(), [
 			'password' => 'required|confirmed'
 		]);
 
-		if($validator->fails()) {
-
-			return \Redirect::to('/register')
-						->withErrors($validator)
-						->withInput();
-		} else {
-			$data = $request->all();
-			$user = Tblaccounts::username_checker($data['username']);
-
-			if(!empty($user)) {
+		$data = $request->all();
+		$user = Tblaccounts::username_checker($data['username']);
+		
+		if($usernamevalidator->fails()){
+				return \Redirect::to('register');
+		}else{
+			if(!empty($user)){ 
 				Session::flash('errmsg1', 'Username is taken.');
 				return \Redirect::to('register');
-			} if($validator1->fails()) {
+			}else if($pwlengthvalidator->fails()) {
 				Session::flash('errmsg2', 'Password must be at least 8 characters.');
 				return \Redirect::to('register');
-			} if($validator2->fails()) {
+			} else if($pwconfirmvalidator->fails()) {
 				Session::flash('errmsg3', 'Password does not match.');
 				return \Redirect::to('register');
-			} else{
+			} else {
 				TblAccounts::add_account($data);
 				return \Redirect::to('/');
+
+				$user = User::create([
+					'username' => $data['username'],
+					//'email' => $data['email'],
+					'password' => $data['password']
+				]);
+
+				Auth::login($user);
 			}
+		}
 
-			 
-
-			$user = User::create([
-				'username' => $data['username'],
-				//'email' => $data['email'],
-				'password' => $data['password']
-			]);
-
-			Auth::login($user);
-			
-		}		
 	}
 }
