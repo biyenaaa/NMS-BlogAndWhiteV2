746800cacd22ff62808d672e2715f4500c9eb588
Aika <aikaviend@gmail.com>
2018-07-03 10:29:56 +0800

    #14 altered the size of the text area.

--- BlogAndWhite/resources/views/posts_show.blade.php
+++ BlogAndWhite/resources/views/posts_show.blade.php
@@ -10,7 +10,7 @@
 	<!-- <div style="padding-top: 20px;" class="col col-sm-10 card-group container form-group"> -->
 		<label for="exampleFormControlTextarea1 container"><h1>{{ $post->title }}</h1></label>
 		<span class="align-middle">by: {{$post->username}}</span>
-		<textarea readonly class="form-control rounded-0 " id="exampleFormControlTextarea1" rows="10">{{$post->content}}</textarea>
+		<textarea readonly class="form-control rounded-0 " id="exampleFormControlTextarea1" rows="18">{{$post->content}}</textarea>
 		<span class="float-sm-right">
 			Date Posted: {{$post->date_published}}
 		</span>
